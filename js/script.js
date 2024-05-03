497c90c2bc68382a2b70d555307592119015afa3
DanielWorkBitrix <d37775989@gmail.com>
2024-05-03 16:59:58 +0300

    version for_home_work

--- js/script.js
+++ js/script.js
@@ -6,3 +6,36 @@ window.addEventListener('scroll', (e) => {
 		document.querySelector('header').classList.remove('scrolled');
 	}
 })
+let menu_btn = document.querySelector('.hamburger');
+
+menu_btn.addEventListener('click', () => {
+	let ul = document.querySelector('ul');
+	let li = document.querySelector('li');
+
+	function hasClass(el, clss) {
+		return el.classList.contains(clss);
+	}
+	if (menu_btn.classList.contains("is-active")) {
+		if(window.innerWidth > 361) {
+			ul.style.top = '-240px';
+		}else {
+			ul.style.top = '-45%';
+		}
+		menu_btn.classList.remove('is-active');
+
+
+	} else {
+		menu_btn.classList.add('is-active');
+		ul.style.top = '80px';
+
+		ul.addEventListener('click', () => {
+			if(window.innerWidth > 361) {
+				ul.style.top = '-240px';
+			}else {
+				ul.style.top = '-45%';
+			}
+			menu_btn.classList.remove('is-active');
+
+		});
+	}
+});
